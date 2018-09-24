d924aaba5ed86d766878f6e4821fbc255dd14310
Maxime BAUMANN <maxime.baumann.pro@gmail.com>
2018-09-24 21:13:03 +0200

    Exo

--- 2_awscli/create-ec2.sh
+++ 2_awscli/create-ec2.sh
@@ -6,14 +6,21 @@ set -e
 # Variable definition
 ######################################################################
 
+EC2_AMI_NAME="ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server*"
+EC2_SUBNET_REGION="us-east-1b"
+EC2_GROUP_NAME="ops"
+EC2_INSTANCE_TYPE="t2.micro"
+EC2_KEY_NAME="aws-360-factory"
 
 ######################################################################
 # Get the AMI_ID
 ######################################################################
 
 echo "Getting AMI Id..."
-
-# TODO
+EC2_AMI_ID=$(aws ec2 describe-images \
+                --filters "Name=name,Values=${EC2_AMI_NAME}" \
+                --query "reverse(sort_by(Images[], &CreationDate))[0].ImageId" \
+                --output text)
 
 echo "Got AMI Id: ${EC2_AMI_ID}"
 
@@ -22,8 +29,9 @@ echo "Got AMI Id: ${EC2_AMI_ID}"
 ######################################################################
 
 echo "Getting VPC Id..."
-
-### TODO
+EC2_VPC_ID=$(aws ec2 describe-vpcs \
+                --filters "Name=isDefault,Values=true" \
+                --query "Vpcs[0].VpcId" --output text)
 
 echo "Got VPC Id: ${EC2_VPC_ID}"
 
@@ -32,8 +40,11 @@ echo "Got VPC Id: ${EC2_VPC_ID}"
 ######################################################################
 
 echo "Getting Subnet Id..."
-
-### TODO
+EC2_SUBNET_ID=$(aws ec2 describe-subnets \
+                    --filters "Name=availabilityZone,Values=${EC2_SUBNET_REGION}" \
+                              "Name=vpcId,Values=${EC2_VPC_ID}" \
+                    --query "Subnets[0].SubnetId" \
+                    --output text)
 
 echo "Got Subnet Id: ${EC2_SUBNET_ID}"
 
@@ -42,8 +53,11 @@ echo "Got Subnet Id: ${EC2_SUBNET_ID}"
 ######################################################################
 
 echo "Getting Security Group Id..."
-
-### TODO
+EC2_SG_ID=$(aws ec2 describe-security-groups \
+                --filters "Name=vpc-id,Values=${EC2_VPC_ID}" \
+                --group-names "${EC2_GROUP_NAME}" \
+                --query "SecurityGroups[0].GroupId" \
+                --output text)
 
 echo "Got Security Group Id: ${EC2_SG_ID}"
 
@@ -52,8 +66,13 @@ echo "Got Security Group Id: ${EC2_SG_ID}"
 ######################################################################
 
 echo "Provisioning EC2 instance..."
-
-### TODO
+EC2_PROVISION=$(aws ec2 run-instances \
+                    --image-id "${EC2_AMI_ID}" \
+                    --key-name "${EC2_KEY_NAME}" \
+                    --instance-type "${EC2_INSTANCE_TYPE}" \
+                    --security-group-ids "${EC2_SG_ID}" \
+                    --subnet-id "${EC2_SUBNET_ID}" \
+                    --output text)
 
 echo "Ec2 Instance ready, here are the details:"
 echo ${EC2_PROVISION}
